a4aacfd3d06ea26a4d63e354834334a231dee81e
Shaikh Ansar <skansar9731@gmail.com>
2024-07-21 20:39:26 +0530

    react Toastify added in the card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,51 +4,57 @@ import Card from "react-bootstrap/Card";
 import Cardsdata from "./CardsData";
 import "./style.css";
 import { useDispatch } from "react-redux";
-import {ADD} from '../../src/redux/actions/action'
+import { ADD } from '../../src/redux/actions/action';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Cards = () => {
   const [data, setData] = useState(Cardsdata);
-  // console.log(data)
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch()
+  const send = (element) => {
+    dispatch(ADD(element));
+    toast.success("Item added to cart !", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  };
 
-
-const send =(e)=>{
-// console.log(e)
-
-dispatch(ADD(e));
-
-}
   return (
     <div className="container mt-3">
-      <h2 className="text-center">Add to Cardsart Project</h2>
-
+      <h2 className="text-center">Add to Cart Project</h2>
       <div className="row d-flex justify-content-center align-items-center ">
         {data.map((element, id) => {
           return (
-            <>
-              <Card
-                style={{ width: "22rem", border: "none" }}
-                className="mx-2 mt-4 card_style"
-              >
-                <Card.Img
-                  variant="top"
-                  src={element.imgdata}
-                  style={{ height: "16rem" }}
-                  className="mt-3"
-                />
-                <Card.Body>
-                  <Card.Title>{element.rname}</Card.Title>
-                  <Card.Text>Price : ₹ {element.price}</Card.Text>
-                  <div className="button_div d-flex justify-content-center">
-                    <Button variant="primary" className="col-lg-12" onClick={()=> send(element)}>Add to Cart</Button>
-                  </div>
-                </Card.Body>
-              </Card>
-            </>
+            <Card
+              key={id}
+              style={{ width: "22rem", border: "none" }}
+              className="mx-2 mt-4 card_style"
+            >
+              <Card.Img
+                variant="top"
+                src={element.imgdata}
+                style={{ height: "16rem" }}
+                className="mt-3"
+              />
+              <Card.Body>
+                <Card.Title>{element.rname}</Card.Title>
+                <Card.Text>Price : ₹ {element.price}</Card.Text>
+                <div className="button_div d-flex justify-content-center">
+                  <Button variant="primary" className="col-lg-12" onClick={() => send(element)}>
+                    Add to Cart
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
           );
         })}
       </div>
+      <ToastContainer />
     </div>
   );
 };
